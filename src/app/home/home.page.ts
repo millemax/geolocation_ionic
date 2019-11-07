a85a01f170d3d6c01c4cdbef2df9d8f4f55798ee
millermax <romel_23_3@hotmail.com>
2019-11-07 17:09:56 -0500

    geolocalicacion ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  lat:number;
+  lng:number;
 
-  constructor() {}
+  constructor(private geolocation: Geolocation) {}
+   
+  capturarlnglat(){
 
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat=resp.coords.latitude;
+      this.lng=resp.coords.longitude;
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+     
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+     });
+  }
+
+    
+
+  
 }
